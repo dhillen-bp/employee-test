e9b990470f9d0a3168844d4207bf577b510001ed
dhillenbp179 <dhillen.mutu@gmail.com>
2024-11-05 11:33:22 +0700

    feat: add model & migration

---
+++ app/Models/Employee.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'hire_date',
+        'position_id',
+        'salary',
+        'status',
+        'photo'
+    ];
+}
